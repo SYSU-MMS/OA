071682d922370f8cdda449aa74a094dde98cfbc2
yangyunguo <13680956981@163.com>
2016-05-11 16:34:30 +0800

    config.php更新

--- mmsoa/application/config/config.php
+++ mmsoa/application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/MOA/mmsoa';
+$config['base_url'] = 'http://localhost/OA/mmsoa';
 
 /*
 |--------------------------------------------------------------------------
