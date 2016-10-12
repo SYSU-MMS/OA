c8abc38a6ac63b05cc139219d90706dbe4bbd13f
MtZero <zhonglsh7@mail2.sysu.edu.cn>
2016-10-12 22:01:15 +0800

    update batch edit

--- mmsoa/assets/js/batch_edit_working_time.js
+++ mmsoa/assets/js/batch_edit_working_time.js
@@ -5,3 +5,7 @@ $(function(){
     $("#select_worker").css("width","100%");
     $("#select_worker_chosen").css("width","100%");
 });
+
+$("#reward_group_button").click(function(){
+    var val=$("#select_worker").val();
+});
