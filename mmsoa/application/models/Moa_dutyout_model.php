f1b466f679b8ec04a568d3a955755f9ae356ddea
MtZero <zhonglsh7@mail2.sysu.edu.cn>
2016-11-21 09:49:50 +0800

    .

--- mmsoa/application/models/Moa_dutyout_model.php
+++ mmsoa/application/models/Moa_dutyout_model.php
@@ -69,7 +69,7 @@ class Moa_dutyout_model extends CI_Model
             $query = $this->db->query("select * from `moa_dutyout` order by `doid` DESC");
             //$this->db->order_by('doid','DESC');
             //$this->db->select();
-            $result = $query->result();
+            $result = $query;
             //echo json_encode(array("status" => true, "data" => $result));
             return $result;
         } else {
