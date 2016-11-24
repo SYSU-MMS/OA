feb772ca6b080aca4549918649c0e149161e3b2a
高少彬 <464590569@qq.com>
2016-11-24 17:06:01 +0800

    fix bug

--- mmsoa/application/config/config.php
+++ mmsoa/application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'htpp://127.0.0.1/OA/mmsoa';
+$config['base_url'] = 'http://127.0.0.1/OA/mmsoa';
 
 /*
 |--------------------------------------------------------------------------
@@ -358,7 +358,7 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = "/Users/crystal/Sites/OA/mmsoa/session";
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
