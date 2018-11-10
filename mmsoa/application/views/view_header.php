c0c74778314d38a90ec45f016b444b34995b7359
AnselDai <ansel_dai@163.com>
2018-11-10 10:48:37 +0800

    Add files via upload

--- mmsoa/application/views/view_header.php
+++ mmsoa/application/views/view_header.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 			<div class="row border-bottom">
                 <nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
                     <div class="navbar-header">
@@ -21,28 +20,4 @@
                     </ul>
 
                 </nav>
-=======
-			<div class="row border-bottom">
-                <nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
-                    <div class="navbar-header">
-                        <a id="mini" class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
-                        <form role="search" class="navbar-form-custom" method="post" action="search_results.html">
-                            <div class="form-group">
-                            	<div class="form-control" >
-		                            
-		                        </div>
-                            </div>
-                        </form>
-                    </div>
-                    <ul class="nav navbar-top-links navbar-right">
-
-                        <li>
-                            <a href="<?php echo site_url('Login'); ?>">
-                                <i class="fa fa-sign-out"></i> 退出
-                            </a>
-                        </li>
-                    </ul>
-
-                </nav>
->>>>>>> abnormal
             </div>
