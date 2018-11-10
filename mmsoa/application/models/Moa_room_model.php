035f8121e60ea5f8e00c2fc1580aecce886a254a
AnselDai <ansel_dai@163.com>
2018-11-10 10:46:51 +0800

    Add files via upload

--- mmsoa/application/models/Moa_room_model.php
+++ mmsoa/application/models/Moa_room_model.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 class Moa_room_model extends CI_Model {
 	/**
@@ -59,65 +58,3 @@ class Moa_room_model extends CI_Model {
 		return $this->db->query($sql)->result();
 	}
 }
-=======
-<?php
-class Moa_room_model extends CI_Model {
-	/**
-	 * 添加课室
-	 * @param paras - 参数列表
-	 * @return 课室的的roomid
-	 */
-	public function add($paras) {
-		if (isset($paras)) {
-			$this->db->insert("MOA_CheckRoom", $paras);
-			return $this->db->insert_id();
-		}
-		else {
-			return false;
-		}
-	}
-
-	/**
-	 * 获取指定roomid的课室信息
-	 * @param roomid - 课室id
-	 * @return roomid对应课室的所有信息
-	 */
-	public function get($roomid) {
-		if (isset($roomid)) {
-			$this->db->where(array('roomid'=>$roomid));
-			$res = $this->db->get('MOA_CheckRoom')->result();
-			return $res[0];
-		}
-		else {
-			$res = $this->db->get('MOA_CheckRoom')->result();
-			return $res;
-		}
-		return false;
-	}
-
-	/**
-	 * 获取指定状态的课室信息
-	 * @param state - 课室state
-	 * @return roomid对应课室的所有信息
-	 */
-	public function get_by_state($state) {
-		if (isset($state)) {
-			$this->db->where(array('state'=>$state));
-			$this->db->order_by('room', 'ASC');
-			return $this->db->get('MOA_CheckRoom')->result();
-		}
-		else {
-			return false;
-		}
-	}
-
-	/**
-	 * 获取所有可选的课室信息
-	 * @return 包含roomid,room, state的课室信息
-	 */
-	public function get_all() {
-		$sql = 'select roomid, room, state from MOA_CheckRoom;';
-		return $this->db->query($sql)->result();
-	}
-}
->>>>>>> abnormal
